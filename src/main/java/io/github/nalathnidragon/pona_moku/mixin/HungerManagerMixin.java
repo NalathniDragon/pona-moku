4bec74a444a0486fb026964fd048d2be761ba06a
RypoFalem <rypofalem@gmail.com>
2023-08-24 10:50:07 -0500

    Eating heals hp
    The naturalRegeneration gamerule is still respected. (and still enabled by default, but that should change)

--- src/main/java/io/github/nalathnidragon/pona_moku/mixin/HungerManagerMixin.java
+++ src/main/java/io/github/nalathnidragon/pona_moku/mixin/HungerManagerMixin.java
@@ -2,32 +2,67 @@ package io.github.nalathnidragon.pona_moku.mixin;
 
 import net.minecraft.entity.player.HungerConstants;
 import net.minecraft.entity.player.HungerManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // For now, set player hunger to max every tick
 @Mixin(HungerManager.class)
 public abstract class HungerManagerMixin {
 
 	@Shadow
-	private int foodLevel;
+	public abstract void setFoodLevel(int foodLevel);
 
 	@Shadow
-	private float foodSaturationLevel;
+	public abstract void setSaturationLevel(float saturationLevel);
+
+	// I don't think these needs to be serialized because they should be modified and reset in the same tick.
+	// The queued saturation and food values are very much intended to be transient.
+	@Unique
+	private float queuedSaturation = 0;
+	@Unique
+	private int queuedFood = 0;
 
 	@Inject(method = "add(IF)V", at = @At("RETURN"))
-	public void add(CallbackInfo info) {
-		foodLevel = HungerConstants.FULL_FOOD_LEVEL;
-		foodSaturationLevel = HungerConstants.MAX_SATURATION;
+	public void add(int food, float saturationModifier, CallbackInfo info) {
+		// kinda duplicated from update but I guess this keeps things from adding negative values
+		setFoodLevel(HungerConstants.FULL_FOOD_LEVEL);
+		setSaturationLevel(HungerConstants.MAX_SATURATION);
+	}
+
+	@Inject(method = "eat(Lnet/minecraft/item/Item;Lnet/minecraft/item/ItemStack;)V", at = @At("RETURN"))
+	public void eat(Item item, ItemStack itemStack, CallbackInfo info) {
+		if(item.isFood()) {
+			FoodComponent food = item.getFoodComponent();
+			if (food != null) { // it shouldn't if item.isFood() but meh.
+				queuedFood += food.getHunger();
+				queuedSaturation += food.getSaturationModifier();
+			}
+		}
 	}
 
 	// Ignore whatever normally happens every tick and set hunger and saturation to max before the method returns
+	// TODO: This should skip vanilla code instead? As is, it respects the 'naturalRegeneration' gamerule but that could
+	// be done manually.
 	@Inject(method = "update(Lnet/minecraft/entity/player/PlayerEntity;)V", at = @At("RETURN"))
-	public void update(CallbackInfo info) {
-		foodLevel = HungerConstants.FULL_FOOD_LEVEL;
-		foodSaturationLevel = HungerConstants.MAX_SATURATION;
+	public void update(PlayerEntity player, CallbackInfo info) {
+		setFoodLevel(HungerConstants.FULL_FOOD_LEVEL);
+		setSaturationLevel(HungerConstants.MAX_SATURATION);
+		player.heal(queuedFood + queuedSaturation); // TODO: Configurable healing
+		queuedSaturation = 0;
+		queuedFood = 0;
+	}
+
+	@Inject(method = "isNotFull()Z", at = @At("RETURN"), cancellable = true)
+	public void isNotFull(CallbackInfoReturnable<Boolean> returnable) {
+		returnable.setReturnValue(true); // always not full. Always ready to eat more.
 	}
 }
