01faf80d20e633e01e6fa8cedec3ebaa0f9c572f
RypoFalem <rypofalem@gmail.com>
2023-08-23 20:06:49 -0500

    Use mod name in logs.
    I went with lowercase "pona moku" because that's how it is presented in the readme.

--- src/main/java/io/github/nalathnidragon/pona_moku/PonaMoku.java
+++ src/main/java/io/github/nalathnidragon/pona_moku/PonaMoku.java
@@ -9,7 +9,7 @@ public class PonaMoku implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("pona moku");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
