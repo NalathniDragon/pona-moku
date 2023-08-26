a01a4ba9fd06f3472e92b5ecba27650a6c407536
NalathniDragon <nmccoy@nmccoy.net>
2023-08-26 13:37:08 -0700

    Update FoodProcessor.java
    Made adjustable parameters private and wrote methods for derived values based on them. Added methods to be used in the appropriate places for handling statuses.

--- src/main/java/io/github/nalathnidragon/pona_moku/FoodProcessor.java
+++ src/main/java/io/github/nalathnidragon/pona_moku/FoodProcessor.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,14 +14,50 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FoodProcessor {
-	public static float HEALTH_SCALE = 1;
-	public static float ABSORPTION_SCALE = 1;
+	private static float HEALTH_SCALE = 1;
+	private static float ABSORPTION_SCALE = 1;
+	private static float MAX_ABSORPTION_FROM_FOOD = 20;
 
-	public static Map<Item,Map<StatusEffect,Integer>> staticFoodBuffs;
+	private static int MIN_EAT_TIME = 8;
+	private static float EAT_TIME_PER_ABSORPTION = 8;
+	private static float EAT_TIME_PER_HEAL = 0; //defaults: less hearty foods are quicker to eat
+	private static Map<Item,Map<StatusEffect,Integer>> staticFoodBuffs;
 
 	static {
 		staticFoodBuffs = new HashMap<>();
 		//TODO: load these from a config file of e.g. {"minecraft:cookie": {"minecraft:speed": 1}}
+		// future: for item IDs missing from the config, derive statuses from ingredient effect strengths in recipe tree
+	}
+	public static void reloadConfig()
+	{
+		//TODO: figure out how to get Item and StatusEffect instances from IDs in the config file
+	}
+
+	public static Collection<StatusEffectInstance> getStatusInstancesFrom(ItemStack stack){
+		Collection<StatusEffectInstance> statuses = new ArrayList<StatusEffectInstance>();
+		//TODO: itemstack's dynamic statuses should preempt static statuses from food type
+		Map<StatusEffect,Integer> staticEffects = staticFoodBuffs.get(stack.getItem());
+		if(staticEffects != null){
+			for(StatusEffect s:staticEffects.keySet())
+			{
+				statuses.add(new StatusEffectInstance(s, StatusEffectInstance.INFINITE_DURATION,staticEffects.get(s)));
+			}
+		}
+		return statuses;
+	}
+	public static float healingFrom(FoodComponent food)
+	{
+		return food.getHunger() * HEALTH_SCALE;
+	}
+	public static float absorptionFrom(FoodComponent food)
+	{
+		return Math.min(food.getHunger() * food.getSaturationModifier() * ABSORPTION_SCALE, MAX_ABSORPTION_FROM_FOOD);
+	}
+
+	//TODO: inject into Item.getMaxUseTime
+	public static int eatTime(FoodComponent food)
+	{
+		return Math.round(Math.max(MIN_EAT_TIME, healingFrom(food) * EAT_TIME_PER_HEAL + absorptionFrom(food) * EAT_TIME_PER_ABSORPTION));
 	}
 
 	public static boolean isFoodEffect(StatusEffectInstance effect, LivingEntity statusHaver)
@@ -50,14 +87,21 @@ public class FoodProcessor {
 			FoodComponent food = item.getFoodComponent();
 			//If they have the absorption effect, clear it so that it doesn't mess with the food's absorption when it expires
 			eater.removeStatusEffect(StatusEffects.ABSORPTION);
-			eater.setAbsorptionAmount(food.getHunger() * food.getSaturationModifier() * ABSORPTION_SCALE);
-			eater.heal(food.getHunger() * HEALTH_SCALE);
+			eater.setAbsorptionAmount(absorptionFrom(food));
+			eater.heal(healingFrom(food));
 			return true;
 		}
 		else
 		{
 			PonaMoku.LOGGER.error("tried to apply food health from non-food item "+item);
 			return false;
+		}
+	}
+	public static void applyFoodStatusesToEntity(ItemStack stack, LivingEntity eater)
+	{
+		Collection<StatusEffectInstance> statuses = getStatusInstancesFrom(stack);
+		for(StatusEffectInstance instance:statuses){
+			eater.addStatusEffect(instance);
 		}
 	}
 }
